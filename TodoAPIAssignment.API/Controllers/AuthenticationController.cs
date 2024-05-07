53a565bb6426abceae1712ca88d1c7e5cf1ff0c9
p18184 <p18184@unipi.gr>
2024-05-07 09:47:54 +0300

    Added SignUp API Endpoint
    I added a signup endpoint that allows the user using an API client like postman to be able to communicate with the API and ask for authentication. The endpoint also returns appropriate status codes for different edge cases.

---
+++ TodoAPIAssignment.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoAPIAssignment.API.Models.RequestModels;
+using TodoAPIAssignment.AuthenticationLibrary;
+
+namespace TodoAPIAssignment.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthenticationController : ControllerBase
+{
+    private readonly IAuthenticationDataAccess _authenticationDataAccess;
+
+    public AuthenticationController(IAuthenticationDataAccess authenticationDataAccess)
+    {
+        _authenticationDataAccess = authenticationDataAccess;
+    }
+
+    [HttpPost("SignUp")]
+    public async Task<IActionResult> SignUp([FromBody] SignUpRequestModel signUpRequestModel)
+    {
+        try
+        {
+            string result = await _authenticationDataAccess.SignUpAsync(signUpRequestModel.Username!, 
+                signUpRequestModel.Password!, signUpRequestModel.Email!)!;
+            
+            if(result == "DuplicateUsernameError")
+                return BadRequest(new {ErrorMessage = "DuplicateUsernameError"});
+            else if (result == "DuplicateEmailError")
+                return BadRequest(new { ErrorMessage = "DuplicateEmailError" });
+
+            return Ok(new { Token = result });
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, "Internal Server Error"); ;
+        }
+    }
+}
