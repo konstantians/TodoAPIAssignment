3dba1b3c9ce7b5acda85359ab174e1b91e9e96bd
p18184 <p18184@unipi.gr>
2024-05-16 23:19:31 +0300

    Added Implementation For Log Out
    Added a basic implementation for log out. This implementation first validates and decodes the token to make sure the user can log out. Once that is done all the existing tokens are put in a blacklist by incrementing by 1 the user specific counter that exists in the database(each user has 1 once registered). Because in the validation there is a check that checks that specific field all previous tokens with the previous counter number will fail if used to access any resources of the REST API after a logout process has occured. After some thought I also added an extra test that checks for this very specific case, which passes(the other 3 pass too) and thus confirms that this implementation works.

--- TodoAPIAssignment.AuthenticationLibrary/IAuthenticationDataAccess.cs
+++ TodoAPIAssignment.AuthenticationLibrary/IAuthenticationDataAccess.cs
@@ -1,9 +1,12 @@
-﻿using TodoAPIAssignment.AuthenticationLibrary.Models;
+﻿using TodoAPIAssignment.AuthenticationLibrary.Enums;
+using TodoAPIAssignment.AuthenticationLibrary.Models;
 
 namespace TodoAPIAssignment.AuthenticationLibrary;
 
 public interface IAuthenticationDataAccess
 {
+    Task<AppUser?> CheckAndDecodeAccessTokenAsync(string accessToken);
     Task<AuthenticationResult>? LogInAsync(string username, string password);
+    Task<ErrorCode> LogOutAsync(string accessToken);
     Task<AuthenticationResult>? SignUpAsync(string username, string password, string email);
 }
