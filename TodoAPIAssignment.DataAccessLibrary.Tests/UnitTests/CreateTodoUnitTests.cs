6a6b6d8e6630ba3f6ccbbff5b75c10649c01af82
p18184 <p18184@unipi.gr>
2024-05-17 20:54:55 +0300

    Added Failing Unit Tests For Create Todo Functionality
    I added 2 unit tests of which 1 tests for the edge case that the database is down/throws exception and the other test checks the cenario in which the todo should be created successfully. In the future additional tests might be added, but for now these are all I can think of.

---
+++ TodoAPIAssignment.DataAccessLibrary.Tests/UnitTests/CreateTodoUnitTests.cs
@@ -0,0 +1,32 @@
+﻿namespace TodoAPIAssignment.DataAccessLibrary.Tests.UnitTests;
+
+[TestFixture]
+[Category("Unit")]
+[FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+[Author("konstantinos", "kinnaskonstantinos0@gmail.com")]
+public class CreateTodoUnitTests
+{
+    [SetUp]
+    public void Setup()
+    {
+
+    }
+
+    [Test]
+    public async Task CreateTodo_ShouldFailAndReturnError_IfDatabaseIsDown()
+    {
+        Assert.Fail();
+    }
+
+    [Test]
+    public async Task CreateTodo_ShouldSucceedAndCreateTodo()
+    {
+        Assert.Fail();
+    }
+
+    [TearDown]
+    public void Teardown() 
+    { 
+    
+    }
+}
