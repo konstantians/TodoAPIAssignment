9c6ffc109126d34e3ab6cfa50c0a41203ad28c6d
p18184 <p18184@unipi.gr>
2024-05-19 19:31:51 +0300

    Added Failing Unit Tests For Get Todos By Id Functionality
    I added 3 basic tests that test the basic of the GetTodoById method. The first 2 tests check the scenarios in which either the todo does not exist or the user does not own it and thus the result should be null and the third test checks the success case. As mentioned before I am not going to check the exception case, because it is too difficult using the in memory database.

---
+++ TodoAPIAssignment.DataAccessLibrary.Tests/UnitTests/GetTodoByIdUnitTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoAPIAssignment.DataAccessLibrary.Tests.UnitTests;
+
+[TestFixture]
+[Category("Unit")]
+[FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+[Author("konstantinos", "kinnaskonstantinos0@gmail.com")]
+public class GetTodoByIdUnitTests
+{
+    private DataDbContext _dataDbContext;
+    private TodoDataAccess _todoDataAccess;
+
+    [SetUp]
+    public void SetUp()
+    {
+        var options = new DbContextOptionsBuilder<DataDbContext>()
+        .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+        .Options;
+
+        _dataDbContext = new DataDbContext(options);
+        _todoDataAccess = new TodoDataAccess(_dataDbContext);
+    }
+
+    [Test]
+    public async Task GetTodoById_ShouldReturnNull_IfTodoNotFound()
+    {
+        Assert.Fail();
+    }
+
+    [Test]
+    public async Task GetTodoById_ShouldReturnNull_IfTodoExistsButUserDoesNotOwnIt()
+    {
+        Assert.Fail();
+    }
+
+    [Test]
+    public async Task GetTodoById_ShouldReturnTodo()
+    {
+        Assert.Fail();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+
+    }
+}
