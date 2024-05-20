d160ef18008876688534596ec8859ed3cf987cfb
p18184 <p18184@unipi.gr>
2024-05-20 15:53:15 +0300

    Added UpdateTodo API Endpoint
    I added an update todo API that allows the user to update one of their todos(Title and IsDone field) using its id. The endpoint also returns appropriate status codes for possible errors.

---
+++ TodoAPIAssignment.API/Models/TodoControllerModels/RequestModels/UpdateTodoRequestModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoAPIAssignment.API.Models.TodoControllerModels.RequestModels;
+
+public class UpdateTodoRequestModel
+{
+    [Required]
+    public string? Id { get; set; }
+    public string? Title { get; set; }
+    public bool IsDone { get; set; }
+}
