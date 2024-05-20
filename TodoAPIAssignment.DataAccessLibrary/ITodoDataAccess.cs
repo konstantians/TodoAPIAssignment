9a5f64b299582bf8468505efb66bb87e267734ae
p18184 <p18184@unipi.gr>
2024-05-20 20:35:24 +0300

    Added DeleteTodo API Endpoint
    I added a delete todo endpoint that allows the user to delete one of their todos using its id. The endpoint also returns appropriate status codes for error case. I also refactor the way the access token is extracted from each method a bit.

--- TodoAPIAssignment.DataAccessLibrary/ITodoDataAccess.cs
+++ TodoAPIAssignment.DataAccessLibrary/ITodoDataAccess.cs
@@ -1,10 +1,12 @@
-﻿using TodoAPIAssignment.DataAccessLibrary.Models;
+﻿using TodoAPIAssignment.DataAccessLibrary.Enums;
+using TodoAPIAssignment.DataAccessLibrary.Models;
 
 namespace TodoAPIAssignment.DataAccessLibrary
 {
     public interface ITodoDataAccess
     {
         Task<CreateTodoResult> CreateTodoAsync(Todo todo);
+        Task<ErrorCode> DeleteUserTodoAsync(string userId, string todoId);
         Task<GetTodoResult> GetUserTodoAsync(string userId, string todoId);
         Task<GetTodosResult> GetUserTodosAsync(string userId);
         Task<UpdateTodoResult> UpdateUserTodoAsync(Todo updatedTodo);
